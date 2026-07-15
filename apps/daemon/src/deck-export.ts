cd506b8ae3c71d5ef0eb69672b4315e81dd351f8
soyeon812 <soyeon@neuralworks.io>
2026-07-15 17:38:28 +0900

    fix(export): add pptx compatibility parts

--- apps/daemon/src/deck-export.ts
+++ apps/daemon/src/deck-export.ts
@@ -47,6 +47,12 @@ function contentTypesXml(images: SlideImage[]): string {
   <Override PartName="/docProps/app.xml" ContentType="application/vnd.openxmlformats-officedocument.extended-properties+xml"/>
   <Override PartName="/docProps/core.xml" ContentType="application/vnd.openxmlformats-package.core-properties+xml"/>
   <Override PartName="/ppt/presentation.xml" ContentType="application/vnd.openxmlformats-officedocument.presentationml.presentation.main+xml"/>
+  <Override PartName="/ppt/presProps.xml" ContentType="application/vnd.openxmlformats-officedocument.presentationml.presProps+xml"/>
+  <Override PartName="/ppt/viewProps.xml" ContentType="application/vnd.openxmlformats-officedocument.presentationml.viewProps+xml"/>
+  <Override PartName="/ppt/tableStyles.xml" ContentType="application/vnd.openxmlformats-officedocument.presentationml.tableStyles+xml"/>
+  <Override PartName="/ppt/slideMasters/slideMaster1.xml" ContentType="application/vnd.openxmlformats-officedocument.presentationml.slideMaster+xml"/>
+  <Override PartName="/ppt/slideLayouts/slideLayout1.xml" ContentType="application/vnd.openxmlformats-officedocument.presentationml.slideLayout+xml"/>
+  <Override PartName="/ppt/theme/theme1.xml" ContentType="application/vnd.openxmlformats-officedocument.theme+xml"/>
   ${slideOverrides}
 </Types>`;
 }
@@ -65,11 +71,18 @@ function presentationXml(count: number, size: { cx: number; cy: number; type: st
     const id = 256 + index;
     return `<p:sldId id="${id}" r:id="rId${index + 1}"/>`;
   }).join('');
+  const masterRelId = `rId${count + 1}`;
   return `<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 <p:presentation xmlns:a="http://schemas.openxmlformats.org/drawingml/2006/main" xmlns:p="http://schemas.openxmlformats.org/presentationml/2006/main" xmlns:r="http://schemas.openxmlformats.org/officeDocument/2006/relationships">
+  <p:sldMasterIdLst><p:sldMasterId id="2147483648" r:id="${masterRelId}"/></p:sldMasterIdLst>
   <p:sldIdLst>${slideIds}</p:sldIdLst>
   <p:sldSz cx="${size.cx}" cy="${size.cy}" type="${size.type}"/>
   <p:notesSz cx="6858000" cy="9144000"/>
+  <p:defaultTextStyle>
+    <a:defPPr>
+      <a:defRPr lang="en-US"/>
+    </a:defPPr>
+  </p:defaultTextStyle>
 </p:presentation>`;
 }
 
@@ -77,8 +90,15 @@ function presentationRelsXml(count: number): string {
   const rels = Array.from({ length: count }, (_value, index) => {
     return `<Relationship Id="rId${index + 1}" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/slide" Target="slides/slide${index + 1}.xml"/>`;
   }).join('');
+  const masterRelId = count + 1;
   return `<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-<Relationships xmlns="http://schemas.openxmlformats.org/package/2006/relationships">${rels}</Relationships>`;
+<Relationships xmlns="http://schemas.openxmlformats.org/package/2006/relationships">
+  ${rels}
+  <Relationship Id="rId${masterRelId}" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/slideMaster" Target="slideMasters/slideMaster1.xml"/>
+  <Relationship Id="rId${masterRelId + 1}" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/presProps" Target="presProps.xml"/>
+  <Relationship Id="rId${masterRelId + 2}" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/viewProps" Target="viewProps.xml"/>
+  <Relationship Id="rId${masterRelId + 3}" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/tableStyles" Target="tableStyles.xml"/>
+</Relationships>`;
 }
 
 function slideXml(index: number, size: { cx: number; cy: number }): string {
@@ -129,7 +149,128 @@ function slideRelsXml(index: number, image: SlideImage): string {
   return `<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 <Relationships xmlns="http://schemas.openxmlformats.org/package/2006/relationships">
   <Relationship Id="rId1" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/image" Target="../media/image${index + 1}.${extension}"/>
+  <Relationship Id="rId2" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/slideLayout" Target="../slideLayouts/slideLayout1.xml"/>
 </Relationships>`;
+}
+
+function slideLayoutXml(): string {
+  return `<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<p:sldLayout xmlns:a="http://schemas.openxmlformats.org/drawingml/2006/main" xmlns:p="http://schemas.openxmlformats.org/presentationml/2006/main" xmlns:r="http://schemas.openxmlformats.org/officeDocument/2006/relationships" type="blank" preserve="1">
+  <p:cSld name="Blank">
+    <p:spTree>
+      <p:nvGrpSpPr>
+        <p:cNvPr id="1" name=""/>
+        <p:cNvGrpSpPr/>
+        <p:nvPr/>
+      </p:nvGrpSpPr>
+      <p:grpSpPr>
+        <a:xfrm>
+          <a:off x="0" y="0"/>
+          <a:ext cx="0" cy="0"/>
+          <a:chOff x="0" y="0"/>
+          <a:chExt cx="0" cy="0"/>
+        </a:xfrm>
+      </p:grpSpPr>
+    </p:spTree>
+  </p:cSld>
+  <p:clrMapOvr><a:masterClrMapping/></p:clrMapOvr>
+</p:sldLayout>`;
+}
+
+function slideLayoutRelsXml(): string {
+  return `<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<Relationships xmlns="http://schemas.openxmlformats.org/package/2006/relationships">
+  <Relationship Id="rId1" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/slideMaster" Target="../slideMasters/slideMaster1.xml"/>
+</Relationships>`;
+}
+
+function slideMasterXml(): string {
+  return `<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<p:sldMaster xmlns:a="http://schemas.openxmlformats.org/drawingml/2006/main" xmlns:p="http://schemas.openxmlformats.org/presentationml/2006/main" xmlns:r="http://schemas.openxmlformats.org/officeDocument/2006/relationships">
+  <p:cSld>
+    <p:spTree>
+      <p:nvGrpSpPr>
+        <p:cNvPr id="1" name=""/>
+        <p:cNvGrpSpPr/>
+        <p:nvPr/>
+      </p:nvGrpSpPr>
+      <p:grpSpPr>
+        <a:xfrm>
+          <a:off x="0" y="0"/>
+          <a:ext cx="0" cy="0"/>
+          <a:chOff x="0" y="0"/>
+          <a:chExt cx="0" cy="0"/>
+        </a:xfrm>
+      </p:grpSpPr>
+    </p:spTree>
+  </p:cSld>
+  <p:clrMap bg1="lt1" tx1="dk1" bg2="lt2" tx2="dk2" accent1="accent1" accent2="accent2" accent3="accent3" accent4="accent4" accent5="accent5" accent6="accent6" hlink="hlink" folHlink="folHlink"/>
+  <p:sldLayoutIdLst><p:sldLayoutId id="2147483649" r:id="rId1"/></p:sldLayoutIdLst>
+  <p:txStyles>
+    <p:titleStyle/>
+    <p:bodyStyle/>
+    <p:otherStyle>
+      <a:defPPr><a:defRPr lang="en-US"/></a:defPPr>
+    </p:otherStyle>
+  </p:txStyles>
+</p:sldMaster>`;
+}
+
+function slideMasterRelsXml(): string {
+  return `<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<Relationships xmlns="http://schemas.openxmlformats.org/package/2006/relationships">
+  <Relationship Id="rId1" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/slideLayout" Target="../slideLayouts/slideLayout1.xml"/>
+  <Relationship Id="rId2" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/theme" Target="../theme/theme1.xml"/>
+</Relationships>`;
+}
+
+function themeXml(): string {
+  return `<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<a:theme xmlns:a="http://schemas.openxmlformats.org/drawingml/2006/main" name="Teamver Design">
+  <a:themeElements>
+    <a:clrScheme name="Teamver">
+      <a:dk1><a:srgbClr val="000000"/></a:dk1>
+      <a:lt1><a:srgbClr val="FFFFFF"/></a:lt1>
+      <a:dk2><a:srgbClr val="1F2937"/></a:dk2>
+      <a:lt2><a:srgbClr val="F8FAFC"/></a:lt2>
+      <a:accent1><a:srgbClr val="2563EB"/></a:accent1>
+      <a:accent2><a:srgbClr val="10B981"/></a:accent2>
+      <a:accent3><a:srgbClr val="F59E0B"/></a:accent3>
+      <a:accent4><a:srgbClr val="EF4444"/></a:accent4>
+      <a:accent5><a:srgbClr val="8B5CF6"/></a:accent5>
+      <a:accent6><a:srgbClr val="06B6D4"/></a:accent6>
+      <a:hlink><a:srgbClr val="2563EB"/></a:hlink>
+      <a:folHlink><a:srgbClr val="7C3AED"/></a:folHlink>
+    </a:clrScheme>
+    <a:fontScheme name="Teamver">
+      <a:majorFont><a:latin typeface="Arial"/><a:ea typeface=""/><a:cs typeface=""/></a:majorFont>
+      <a:minorFont><a:latin typeface="Arial"/><a:ea typeface=""/><a:cs typeface=""/></a:minorFont>
+    </a:fontScheme>
+    <a:fmtScheme name="Teamver">
+      <a:fillStyleLst><a:solidFill><a:schemeClr val="phClr"/></a:solidFill></a:fillStyleLst>
+      <a:lnStyleLst><a:ln w="9525" cap="flat" cmpd="sng" algn="ctr"><a:solidFill><a:schemeClr val="phClr"/></a:solidFill><a:prstDash val="solid"/></a:ln></a:lnStyleLst>
+      <a:effectStyleLst><a:effectStyle><a:effectLst/></a:effectStyle></a:effectStyleLst>
+      <a:bgFillStyleLst><a:solidFill><a:schemeClr val="phClr"/></a:solidFill></a:bgFillStyleLst>
+    </a:fmtScheme>
+  </a:themeElements>
+</a:theme>`;
+}
+
+function presPropsXml(): string {
+  return `<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<p:presentationPr xmlns:p="http://schemas.openxmlformats.org/presentationml/2006/main"/>`;
+}
+
+function viewPropsXml(): string {
+  return `<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<p:viewPr xmlns:p="http://schemas.openxmlformats.org/presentationml/2006/main">
+  <p:normalViewPr><p:restoredLeft sz="15620"/><p:restoredTop sz="94660"/></p:normalViewPr>
+</p:viewPr>`;
+}
+
+function tableStylesXml(): string {
+  return `<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<a:tblStyleLst xmlns:a="http://schemas.openxmlformats.org/drawingml/2006/main" def="{5C22544A-7EE6-4342-B048-85BDC9FD1C3A}"/>`;
 }
 
 function appXml(slideCount: number): string {
@@ -168,6 +309,14 @@ export async function buildScreenshotPptx(
   zip.file('docProps/core.xml', coreXml(opts.title));
   zip.file('ppt/presentation.xml', presentationXml(images.length, size));
   zip.file('ppt/_rels/presentation.xml.rels', presentationRelsXml(images.length));
+  zip.file('ppt/presProps.xml', presPropsXml());
+  zip.file('ppt/viewProps.xml', viewPropsXml());
+  zip.file('ppt/tableStyles.xml', tableStylesXml());
+  zip.file('ppt/slideLayouts/slideLayout1.xml', slideLayoutXml());
+  zip.file('ppt/slideLayouts/_rels/slideLayout1.xml.rels', slideLayoutRelsXml());
+  zip.file('ppt/slideMasters/slideMaster1.xml', slideMasterXml());
+  zip.file('ppt/slideMasters/_rels/slideMaster1.xml.rels', slideMasterRelsXml());
+  zip.file('ppt/theme/theme1.xml', themeXml());
 
   images.forEach((image, index) => {
     const extension = image.jpeg ? 'jpg' : 'png';
--- apps/daemon/tests/deck-export.test.ts
+++ apps/daemon/tests/deck-export.test.ts
@@ -24,11 +24,28 @@ describe('buildScreenshotPptx', () => {
     expect(zip.file('ppt/slides/slide2.xml')).toBeTruthy();
     expect(zip.file('ppt/media/image1.png')).toBeTruthy();
     expect(zip.file('ppt/media/image2.png')).toBeTruthy();
+    expect(zip.file('ppt/slideMasters/slideMaster1.xml')).toBeTruthy();
+    expect(zip.file('ppt/slideLayouts/slideLayout1.xml')).toBeTruthy();
+    expect(zip.file('ppt/theme/theme1.xml')).toBeTruthy();
+    expect(zip.file('ppt/presProps.xml')).toBeTruthy();
+    expect(zip.file('ppt/viewProps.xml')).toBeTruthy();
+    expect(zip.file('ppt/tableStyles.xml')).toBeTruthy();
 
     const presentation = await zip.file('ppt/presentation.xml')!.async('string');
     expect(presentation).toContain('r:id="rId1"');
     expect(presentation).toContain('r:id="rId2"');
+    expect(presentation).toContain('<p:sldMasterIdLst>');
     expect(presentation).toContain('type="screen16x9"');
+
+    const presentationRels = await zip.file('ppt/_rels/presentation.xml.rels')!.async('string');
+    expect(presentationRels).toContain('/relationships/slideMaster');
+    expect(presentationRels).toContain('/relationships/presProps');
+    expect(presentationRels).toContain('/relationships/viewProps');
+    expect(presentationRels).toContain('/relationships/tableStyles');
+
+    const slideRels = await zip.file('ppt/slides/_rels/slide1.xml.rels')!.async('string');
+    expect(slideRels).toContain('/relationships/image');
+    expect(slideRels).toContain('/relationships/slideLayout');
   });
 
   it('rejects empty decks', async () => {
